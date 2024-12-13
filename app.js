2ccb076a7813cd4abe86a5def4ddb565a0a54e2f
Arzoo Shamim <166494063+arzooshamim13@users.noreply.github.com>
2024-12-13 06:30:21 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+console.log("Hello World !");
+
+// starting class 14
+
+
+// DATE AND TIME
+// var time = new Date();
+// DayOfWeek , Month , Date , Year , Minaute , Second , Time Zone
+// var days = ["Sun" , "Mon" , "Tue" , "Wed" , "Thu" , "Fri" , "Sat"]
+// console.log(time.getHours());
+// console.log(time.getMinutes());
+// console.log(time.getSeconds());
+// console.log(time.getMilliseconds());
+// var date = new Date();
+// var gen = new Date()
+// console.log(date);
+
